1b5b78ae7203b20150f468ebddd265fc34989988
Alejandro Arce <>
2023-01-19 10:49:51 +0100

    Remove commented lines

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -16,13 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let navigationController = UINavigationController.init()
         window = UIWindow()
-        //let bottomSafeArea = window!.safeAreaInsets.bottom
-        //appCoordinator = AppCoordinator(navigationController: navigationController,bottomSafeArea: bottomSafeArea)
         appCoordinator = AppCoordinator(navigationController: navigationController)
         appCoordinator.start()
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-        //print(bottomSafeArea)
 
         return true
     }
